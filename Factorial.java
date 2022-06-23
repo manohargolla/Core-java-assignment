004a14d5b636bece8ba6d5b749c1160fde769fe3
manohargolla <108002616+manohargolla@users.noreply.github.com>
2022-06-23 22:49:37 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,12 @@
 public class Factorial {
    public static void main(String []args) {
-	   long factorial=1;
+	   int f=1;
 	   int num=10;
 	  
 	   for(int i=1;i<=num;i++) {
-		   factorial=factorial*i;
+		   f=f*i;
 	   }
-	   System.out.println("The factorial of "+num+" is "+factorial);
+	   System.out.println("The factorial of "+num+" is "+f);
 	   
 	    		
    }
