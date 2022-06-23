f44941cd8a4be45e19b21eff20a77c7a4036c4a8
manohargolla <108002616+manohargolla@users.noreply.github.com>
2022-06-23 22:33:03 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+   public static void main(String []args) {
+	   long factorial=1;
+	   int num=10;
+	  
+	   for(int i=1;i<=num;i++) {
+		   factorial=factorial*i;
+	   }
+	   System.out.println("The factorial of "+num+" is "+factorial);
+	   
+	    		
+   }
+}
