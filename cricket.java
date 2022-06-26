ee9a0ab5f45cbd2b6e1a4ce7fb45861e37dd9b51
manohargolla <108002616+manohargolla@users.noreply.github.com>
2022-06-26 22:24:44 +0530

    Update cricket.java

--- cricket.java
+++ cricket.java
@@ -23,24 +23,24 @@ class cricket {
       System.out.println(firstplayer+"Total score :"+score1);
       System.out.println(firstplayer+"Takes :"+count1+" Rounds");
       System.out.println(secondplayer+" Needs "+(score1+1)+" In "+ count1+" Rounds To WIN MATCH");
-      System.out.println("player :"+firstplayer);
+      System.out.println("player :"+secondplayer);
       do{
           score=random()&7;
           score2=score2+score;
           count2=count2+1;  
         }while(score>0);
       if(score1>score2){
-        System.out.println(firstplayer+"WIN Match !!");
+        System.out.println(firstplayer+" WIN Match !!");
        }
       else if(score2>score1){
-        System.out.println(secondplayer+"WIN Match !!");
+        System.out.println(secondplayer+" WIN Match !!");
       }
       else{
         if(count1>count2){
-          System.out.println(firstplayer+"WIN Match !!");
+          System.out.println(secondplayer+" WIN Match !!");
         }
         else{
-          System.out.println(secondplayer+"WIN Match !!");
+          System.out.println(firstplayer+" WIN Match !!");
         }
       }
      
